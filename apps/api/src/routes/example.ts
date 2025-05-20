1485eb38f171a6036f461451b1acf9ef3750c42f
Yurii Vydai <yuriivydai@gmail.com>
2025-05-20 16:32:25 +0300

    getFiles

--- apps/api/src/routes/example.ts
+++ apps/api/src/routes/example.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { router, publicProcedure } from "./trpc";
 import { testSchema } from "schema";
+import prisma from '../utils/prismaClient';
 
 export const exampleRouter = router({
   example: publicProcedure.input(testSchema).query(({ ctx, input }) => {
@@ -12,5 +13,10 @@ export const exampleRouter = router({
     .query((opts) => {
       const { input } = opts;
       return `Hello ${input.name}` as const;
+    }),
+  getFiles: publicProcedure
+    .query(() => {
+      const allFiles = prisma.file.findMany()
+      return allFiles;
     }),
 });
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,16 +1,44 @@
 import { Button } from "ui";
 import { trpc } from "../utils/trpc";
+import Link from "next/link";
 
 export default function Web() {
   const health = trpc.health.health.useQuery();
   const res = trpc.example.greeting.useQuery({ name: 'John' });
+  const resFiles = trpc.example.getFiles.useQuery();
+
+  console.log(resFiles.data);
+
+  function handleDelete(name: string): void {
+    console.log('delete: ' + name)
+  }
 
   return (
     <div>
       {health.data && <p>TRPC Health: {health.data.health}</p>}
       {res.data && <p>Hello: {res.data}</p>}
-      <h1>Web</h1>
-      <Button />
+      {resFiles.data && <div>
+        <p>Files: </p>
+        <table>
+          <thead>
+            <tr>
+              <th>file</th>
+              <th>actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {resFiles.data.map((file: any) => (
+              <tr key={file.id}>
+                <td>
+                  {file.name}
+                </td>
+                <td>
+                  <button className="btn btn-red" onClick={() => handleDelete(file.name)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table></div>}
     </div>
   );
 }
